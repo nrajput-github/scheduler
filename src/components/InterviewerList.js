318c2e24a7a7fe848aafb05c84786661aa041d95
Navya Rajput <navyasrajput@gmail.com>
2021-10-27 15:18:11 -0400

    Creating Interviewer and Building the Interviewer Item

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+import "components/InterviewerList.scss";
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={event => props.onChange(interviewer.id)}
+      />
+    )
+  });
+
+  return (
+    <fragment>
+      <section className="interviewers">
+        <h4 className="interviewers__header text--light">Interviewer</h4>
+        <ul className="interviewers__list">
+          {interviewers}
+        </ul>
+      </section>
+    </fragment>
+  )
+};
