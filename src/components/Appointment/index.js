4166974ccbdd0d9b0f0a7e193596c8a008c6347e
Navya Rajput <navyasrajput@gmail.com>
2021-10-28 19:20:53 -0400

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
-
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 export default function Appointment(props) {
-  let app;
-  app = `${!props.time ? ' No Appointments' : 'Appointment at 12pm'}`;
   return (
-    <article className="appointment">{app}</article>
-  )
-}
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
+  );
+};
