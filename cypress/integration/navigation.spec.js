967771b01eda4fba4fd83dc5a768406402f331b3
Navya Rajput <navyasrajput@gmail.com>
2021-11-03 22:59:30 -0400

    Add Navigation test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,3 +3,15 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 });
+it("should navigate to Tuesday", () => {
+  cy.visit("/");
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+
+});
+
